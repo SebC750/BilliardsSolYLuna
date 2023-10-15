88c8b0ad35a19942f7566c0051024d03b477e642
SebC750 <SebC89999@gmail.com>
2023-10-15 03:19:55 -0400

    git commit

--- billiardssolyluna/src/pages/Table1.js
+++ billiardssolyluna/src/pages/Table1.js
@@ -1,5 +1,39 @@
+import {
+    Card,
+    Button,
+    Modal,
+    Form,
+    Col,
+    Row,
+    Container,
+  } from "react-bootstrap";
 import pool from "../images/pooltable.png"
+import {useState} from "react"
 const Table1 = () => {
+    const [timeStart, setTimeStart] = useState(false);
+    var totalSeconds = 0;
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    const startTimer = () =>{
+        setTimeStart(true)
+        setTime()
+    }
+
+    const setTime = () =>{
+        
+        totalSeconds++
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+
+    }
+    function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+          return "0" + valString;
+        } else {
+          return valString;
+        }
+      }
     return (
         <div>
             <div class="container">
@@ -8,6 +42,11 @@ const Table1 = () => {
                     
                 </div>
                 <img src={pool}></img>
+                <Button class="btn-success" onClick={() => startTimer()}> Start time</Button>
+                {timeStart ? (
+                    <h3> Time: <label id="minutes">00</label>:<label id="seconds">00</label></h3>
+                ): null}
+                
             </div>
         </div>
     );
